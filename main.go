b526985f7fcc05f793051a79ae345bede9659643
Brian Vanegas <brianvs18@gmail.com>
2024-04-16 22:03:17 -0500

    add new switch sentence for the workshop

--- main.go
+++ main.go
@@ -3,21 +3,23 @@ package main
 import (
 	"fmt"
 	"os"
+	"prueba-tecnica1-labora/bug-tracking-system/presenters"
 	"prueba-tecnica1-labora/fizzbuzz"
-	"prueba-tecnica1-labora/menu-packaging"
-	"prueba-tecnica1-labora/soccer-players"
+	menu_packaging "prueba-tecnica1-labora/menu-packaging"
+	soccer_players "prueba-tecnica1-labora/soccer-players"
 	"time"
 )
 
 func main() {
 	for {
 		var choice int
-		fmt.Println("Seleccione una opción: ")
+		fmt.Println(":: MENU PRINCIPAL :: ")
 		fmt.Println("1. Ejercicio jugadores de futbol")
 		fmt.Println("2. Ejercicio FizzBuzz")
 		fmt.Println("3. Ejercicio Menu Packaging")
 		fmt.Println("4. Ejercicio entregable")
 		fmt.Println("5. Salir")
+		fmt.Print("Seleccione una opción: ")
 		fmt.Scanln(&choice)
 
 		switch choice {
@@ -31,7 +33,7 @@ func main() {
 			menu_packaging.Execute()
 			sleepOneSecond()
 		case 4:
-			fmt.Println("Proximamente")
+			presenters.InitMenu()
 			sleepOneSecond()
 		case 5:
 			fmt.Println("Hasta luego")
